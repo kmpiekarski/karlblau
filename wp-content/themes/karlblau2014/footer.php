386d41a2886dfd00d2bf9bb87b5c2a24c4cf5a92
slashedtires <kenneth.m.piekarski@gmail.com>
2014-06-25 14:41:27 -0700

    Added bootstrap .js calls to footer

--- wp-content/themes/karlblau2014/footer.php
+++ wp-content/themes/karlblau2014/footer.php
@@ -21,5 +21,12 @@
 	</footer><!-- .site-footer -->
 
 	<?php wp_footer(); ?>
+
+  <!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/docs.min.js"></script>
 </body>
 </html>
