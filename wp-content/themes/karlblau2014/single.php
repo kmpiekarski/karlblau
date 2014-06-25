da30bdc5345f9778e8d3843764f7bfdf543fc421
slashedtires <kenneth.m.piekarski@gmail.com>
2014-06-25 15:34:42 -0700

    Removed comment forms

--- wp-content/themes/karlblau2014/single.php
+++ wp-content/themes/karlblau2014/single.php
@@ -25,10 +25,6 @@ get_header(); ?>
 					// Previous/next post navigation.
 					twentyfourteen_post_nav();
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
 				endwhile;
 			?>
 		</div><!-- #content -->
