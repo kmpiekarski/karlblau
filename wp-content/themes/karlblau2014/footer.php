250f5bd3df11fb2b8eca668d89027a8e0cb2df72
slashedtires <kenneth.m.piekarski@gmail.com>
2014-06-25 14:40:16 -0700

    Transferred footer from karlblau-layout

--- wp-content/themes/karlblau2014/footer.php
+++ wp-content/themes/karlblau2014/footer.php
@@ -10,18 +10,15 @@
  */
 ?>
 
-		</div><!-- #main -->
+		</div><!-- #main --><!-- /.row -->
+	</div><!-- #page --><!-- /.container -->
 
-		<footer id="colophon" class="site-footer" role="contentinfo">
-
-			<?php get_sidebar( 'footer' ); ?>
-
-			<div class="site-info">
-				<?php do_action( 'twentyfourteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
-		</footer><!-- #colophon -->
-	</div><!-- #page -->
+	<footer class="site-footer">
+		<?php get_sidebar( 'footer' ); ?>
+    <p>
+      <a href="#">hop to top!</a>
+    </p>
+	</footer><!-- .site-footer -->
 
 	<?php wp_footer(); ?>
 </body>
