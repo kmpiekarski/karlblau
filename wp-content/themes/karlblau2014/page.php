91453a113e2a8bdc28879229f28ffbe687deea04
slashedtires <kenneth.m.piekarski@gmail.com>
2014-06-25 15:02:34 -0700

    Modified page.php template

--- wp-content/themes/karlblau2014/page.php
+++ wp-content/themes/karlblau2014/page.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-<div id="main-content" class="main-content">
+<div class="col-sm-8 site-main">
 
 <?php
 	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
@@ -41,7 +41,7 @@ get_header(); ?>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 	<?php get_sidebar( 'content' ); ?>
-</div><!-- #main-content -->
+</div><!-- #site-main -->
 
 <?php
 get_sidebar();
