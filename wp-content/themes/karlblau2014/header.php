ab0b91df9c37e3d1dc8df81481955a635a59b3d1
slashedtires <kenneth.m.piekarski@gmail.com>
2014-06-25 16:23:14 -0700

    Removed the site-menu container and renamed it's css class to 'menu-main-nav-container'

--- wp-content/themes/karlblau2014/header.php
+++ wp-content/themes/karlblau2014/header.php
@@ -45,9 +45,7 @@
   <div class="site-header">
     <div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
     <div class="container">
-      <nav class="site-nav">
-        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'site-nav-item' ) ); ?>
-      </nav>
+      <?php wp_nav_menu( array( 'theme_location' => 'primary') ); ?>
     </div>
   </div><!-- .site-header -->
 </div><!-- .site-masthead -->
