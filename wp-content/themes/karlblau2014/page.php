f9eada3f6010a83868bf06f846ae2be8b8e2f590
Kenneth M. Piekarski <kenneth.m.piekarski@gmail.com>
2014-06-26 23:39:23 -0700

    Styled a lil bit more and removed unused containers

--- wp-content/themes/karlblau2014/page.php
+++ wp-content/themes/karlblau2014/page.php
@@ -14,29 +14,13 @@
 get_header(); ?>
 
 <div class="col-sm-8 site-main opaque-back">
-
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
-
 					// Include the page content template.
 					get_template_part( 'content', 'page' );
-
 				endwhile;
 			?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
 </div><!-- #site-main -->
 
 <?php
